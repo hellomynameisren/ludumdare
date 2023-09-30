9928bcbaacd84b5c41881321134121316e38fc95
Jessica Taylor <jessi.liu.taylor@gmail.com>
2023-09-29 22:50:23 -0400

    jumping

--- liquid/player.gd
+++ liquid/player.gd
@@ -8,15 +8,14 @@ var gravity = 800
 func get_input():
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	if input_vector != Vector2.ZERO:
-		velocity.x = input_vector.x * speed
+	velocity.x = input_vector.x * speed
 	return input_vector
 
 func _physics_process(delta):
 	var input_vector = get_input()
   
 	if is_on_floor():
-		if Input.is_action_just_pressed("ui_select") and is_on_floor():
+		if Input.is_action_just_pressed("ui_up") and is_on_floor():
 			velocity.y = jump_speed
 		else:
 			velocity.y = 0
