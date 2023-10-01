9a70f8b6d6168c937aeecac164f5f18c57abe116
Jessica Taylor <jessi.liu.taylor@gmail.com>
2023-10-01 07:19:19 -0400

    spider level 2

--- liquid/play_area.gd
+++ liquid/play_area.gd
@@ -8,10 +8,12 @@ class_name play_area
 # var Level2 = preload("res://liquid/levels/test_level2.tscn")
 # var Level3 = preload("res://liquid/levels/test_level3.tscn")
 var levels = [
-	preload("res://liquid/levels/chamber_level.tscn"),
-	preload("res://liquid/levels/gravel_level.tscn"),
-	preload("res://liquid/levels/spider_level.tscn"),
-	preload("res://liquid/levels/test_level.tscn"),
+	preload("res://liquid/levels/chamber_level2.tscn"),
+	preload("res://liquid/levels/spider_level2.tscn"),
+	preload("res://liquid/levels/race_level2.tscn"),
+	#preload("res://liquid/levels/gravel_level.tscn"),
+	#preload("res://liquid/levels/spider_level.tscn"),
+	#preload("res://liquid/levels/test_level.tscn"),
 ]
 
 # var levels = [Level1, Level2, Level3]
