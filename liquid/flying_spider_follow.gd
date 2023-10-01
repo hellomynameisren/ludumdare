f5a94b96caf11b18f1fed4b6107f86f6df62f778
Jessica Taylor <jessi.liu.taylor@gmail.com>
2023-10-01 00:35:40 -0400

    spider animation changes direction

--- liquid/flying_spider_follow.gd
+++ liquid/flying_spider_follow.gd
@@ -3,6 +3,10 @@ extends PathFollow2D
 # Set the speed in pixels per second.
 var speed: float = 100.0  
 
+
+func _ready() -> void:
+	pass
+
 func _process(delta: float) -> void:
 	# var path_length = get_parent().get_path_length()
 	var movement_per_frame = (speed * delta) 
