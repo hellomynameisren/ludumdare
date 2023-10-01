227d985da98120f4334efbb3871147af7bda564f
nobody <netrunnernobody@turbokiller.net>
2023-09-30 22:06:47 -0500

    makes boulders slightly easier to dunk on

--- liquid/Boulder.gd
+++ liquid/Boulder.gd
@@ -1,6 +1,9 @@
 extends RigidBody2D
 
 var push = 0
+var pushes = 1
+var speed = 600
+var winertia = 240
 
 func _ready():
     # Set the initial gravity scale to 0 to make it inert
@@ -11,7 +14,11 @@ func _on_body_entered(body):
     # Check if the body that entered is in the "player" group
     if body.is_in_group("player"):
         # Determine the direction to apply the impulse based on the player's position
-        if push < 3:
-            var impulse_direction = Vector2(-400, 0) if body.position.x < position.x else Vector2(400, 0)
+        if push < pushes:
+            var impulse_direction = Vector2(-speed, 0) if body.position.x < position.x else Vector2(speed, 0)
             apply_central_impulse(impulse_direction)
             push += 1
+            speed *= 2
+        else:
+            var impulse_direction = Vector2(-winertia, 0) if body.position.x < position.x else Vector2(winertia, 0)
+            apply_central_impulse(impulse_direction)
