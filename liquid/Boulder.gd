f6285e2cc65b18aed4f2421c204a205bafd2ef81
nobody <netrunnernobody@turbokiller.net>
2023-09-30 21:47:45 -0500

    bugfixes w boulders

--- liquid/Boulder.gd
+++ liquid/Boulder.gd
@@ -3,8 +3,8 @@ extends RigidBody2D
 var push = 0
 
 func _ready():
-	# Set the initial gravity scale to 0 to make it inert
-	gravity_scale = 1
+    # Set the initial gravity scale to 0 to make it inert
+    gravity_scale = 1
 
 func _on_body_entered(body):
     #print("Boulder triggered")
@@ -12,6 +12,6 @@ func _on_body_entered(body):
     if body.is_in_group("player"):
         # Determine the direction to apply the impulse based on the player's position
         if push < 3:
-            var impulse_direction = Vector2(-500, 0) if body.position.x < position.x else Vector2(500, 0)
+            var impulse_direction = Vector2(-400, 0) if body.position.x < position.x else Vector2(400, 0)
             apply_central_impulse(impulse_direction)
             push += 1
