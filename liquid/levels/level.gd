52525aef3fb4ff6f10a1cd69f13020b1f2c08d07
Jessica Taylor <jessi.liu.taylor@gmail.com>
2023-10-01 07:48:41 -0400

    map to global position

--- liquid/levels/level.gd
+++ liquid/levels/level.gd
@@ -55,41 +55,40 @@ func replace_tiles():
 	for x in range(used_rect.position.x, used_rect.position.x + used_rect.size.x):
 		for y in range(used_rect.position.y, used_rect.position.y + used_rect.size.y):
 			var tile_coord = Vector2(x, y)
+			var local_pos = tilemap.map_to_local(tile_coord + Vector2(1/2, 1/2))
+			var global_pos = tilemap.to_global(local_pos)
 			var tile_id = tilemap.get_cell_source_id(0, tile_coord)
 			# print("tile " + str(tile_id) + " loc " + str(tile_coord))
 			#if tile_id == 0:
 			#	var position = $TileMap.map_to_local(tile_coord + Vector2(1/2, 1/2))
 			#	var wall_instance = wall_scene.instantiate()
-			#	wall_instance.global_position = position
+			#	wall_instance.global_position = global_pos
 			#	self.add_child(wall_instance)
 			#	$TileMap.set_cell(0, tile_coord, -1)
 			if tile_id == 1:
-				var position = tilemap.map_to_local(tile_coord + Vector2(1/2, 1/2))
+				# print("spawn lava at " + str(position))
 				var lava_instance = lava_block_scene.instantiate()
-				lava_instance.global_position = position
+				lava_instance.global_position = global_pos
 				self.add_child(lava_instance)
-				lava_instance.global_position = position
+				lava_instance.global_position = global_pos
 				tilemap.set_cell(0, tile_coord, -1)
 			if tile_id == 2:
 				var frame = tilemap.get_cell_tile_data(0, tile_coord).get_custom_data_by_layer_id(0)
-				var position = tilemap.map_to_local(tile_coord + Vector2(1/2, 1/2))
 				var breakable_instance = breakable_block_scene.instantiate()
-				breakable_instance.global_position = position
+				breakable_instance.global_position = global_pos
 				breakable_instance.get_node("Sprite2D").frame = frame
 				self.add_child(breakable_instance)
-				breakable_instance.global_position = position
+				breakable_instance.global_position = global_pos
 				tilemap.set_cell(0, tile_coord, -1)
 			if tile_id == 3:
-				var position = tilemap.map_to_local(tile_coord + Vector2(1/2, 1/2))
 				var gravel_instance = gravel_scene.instantiate()
-				gravel_instance.global_position = position
+				gravel_instance.global_position = global_pos
 				self.add_child(gravel_instance)
-				gravel_instance.global_position = position
+				gravel_instance.global_position = global_pos
 				tilemap.set_cell(0, tile_coord, -1)
 			if tile_id == 4:
-				var position = tilemap.map_to_local(tile_coord + Vector2(1/2, 1/2))
 				var weak_instance = weak_scene.instantiate()
-				weak_instance.global_position = position
+				weak_instance.global_position = global_pos
 				self.add_child(weak_instance)
-				weak_instance.global_position = position
+				weak_instance.global_position = global_pos
 				tilemap.set_cell(0, tile_coord, -1)
